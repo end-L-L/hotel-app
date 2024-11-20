4ba50dc9889aea2153d01ef4a860fb81e1e04942
Boset <boss-x86@protonmail.com>
2024-11-20 13:48:52 -0600

    feature - dynamic routes
    -  room route implemented

--- src/app/hotel/pages/admin-page/admin-page.component.ts
+++ src/app/hotel/pages/admin-page/admin-page.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { NavbarComponent } from "../../partials/navbar/navbar.component";
 
 // Router
-import { Router} from '@angular/router';
+import { Router, RouterOutlet, RouterLink, NavigationEnd} from '@angular/router';
 
 // Facade Service
 import { FacadeService } from '../../../auth/services/facade.service';
@@ -21,7 +21,8 @@ const MaterialModules = [
   standalone: true,
   imports: [
     MaterialModules,
-    NavbarComponent
+    NavbarComponent,
+    RouterOutlet,
   ],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss',
@@ -30,18 +31,33 @@ const MaterialModules = [
 export class AdminPageComponent implements OnInit {
 
   public name_user: string = '';
-  
+  showDashboard: boolean = true;
+  currentRoute: string | undefined;
+
   constructor(
     private facadeService: FacadeService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
+
     this.name_user = this.facadeService.getUsername();
+
+    this.showDashboard = this.router.url === '/hotel';
+    
+    console.log(this.router.config);
+    this.currentRoute = this.router.url;
+    console.log('Ruta actual:', this.currentRoute);
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showDashboard = event.url === '/hotel';
+      }
+    });
   }
 
   options = [
-    { name: 'Habitaciones', icon: 'assets/icons/hotel-habitaciones.png', link: '/' },
+    { name: 'Habitaciones', icon: 'assets/icons/hotel-habitaciones.png', link: '/hotel/rooms' },
     { name: 'Tipo de Habitaciones', icon: 'assets/icons/hotel-tipo.png', link: '/' },
     { name: 'Reservas', icon: 'assets/icons/hotel-reserva.png', link: '/' },
     { name: 'Añadir Reserva', icon: 'assets/icons/hotel-añadir-reserva.png', link: '/' },
@@ -53,6 +69,7 @@ export class AdminPageComponent implements OnInit {
 
   navigateTo(route: string) {
     this.router.navigate([route]);
+    console.log(route);
   }
 
 }
