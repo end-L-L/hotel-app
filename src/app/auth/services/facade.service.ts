ebb790a4d3887645758762c471eb913870938bea
Boset <boss-x86@protonmail.com>
2024-11-20 01:42:50 -0600

    feature - facade login
    - implemented ngx-cookie-service
    - implemented cookies

--- src/app/auth/services/facade.service.ts
+++ src/app/auth/services/facade.service.ts
@@ -1,6 +1,22 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Injectable } from '@angular/core';
-import { ValidatorService } from './extensions/validator.service';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { ErrorsService } from './extensions/errors.service';
+import { ValidatorService } from './extensions/validator.service';
+
+import { environment } from '@env/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+// cookies names
+const user_id_cookie_name = 'hotel-user_id';
+const user_email_cookie_name = 'hotel-user_email';
+const session_cookie_name = 'hotel-user_token';
+const group_name_cookie_name = 'hotel-user_group-name';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +24,8 @@ import { ErrorsService } from './extensions/errors.service';
 export class FacadeService {
 
   constructor(
+    private http: HttpClient,
+    private cookieService: CookieService,
     private validatorService: ValidatorService,
     private errorsService: ErrorsService
   ) { }
@@ -31,5 +49,32 @@ export class FacadeService {
     }
 
     return error;
+  }
+
+  login(username:String, password:String): Observable<any> {
+    var data={
+      username: username,
+      password: password
+    }
+    return this.http.post<any>(`${environment.API_URL}/api/v1/login`,data);
+  }
+
+  // session cookie
+  saveUserData(user_data:any){
+    var secure = environment.API_URL.indexOf("https")!=-1;
+
+    if(user_data.role == "administrador"){
+      console.log("administrador");
+      this.cookieService.set(user_id_cookie_name, user_data.id, undefined, undefined, undefined, secure, secure?"None":"Lax");
+      this.cookieService.set(user_email_cookie_name, user_data.email, undefined, undefined, undefined, secure, secure?"None":"Lax");  
+    }
+
+    if(user_data.role == "recepcionista"){
+      this.cookieService.set(user_id_cookie_name, user_data.user.id, undefined, undefined, undefined, secure, secure?"None":"Lax");
+      this.cookieService.set(user_email_cookie_name, user_data.user.email, undefined, undefined, undefined, secure, secure?"None":"Lax");
+    }
+
+    this.cookieService.set(session_cookie_name, user_data.token, undefined, undefined, undefined, secure, secure?"None":"Lax");
+    this.cookieService.set(group_name_cookie_name, user_data.role, undefined, undefined, undefined, secure, secure?"None":"Lax");
   }
 }
