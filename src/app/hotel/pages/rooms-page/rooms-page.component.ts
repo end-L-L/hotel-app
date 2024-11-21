15326aa3f35b13e82dbdd999a9b40b7bc4ac74db
Boset <boss-x86@protonmail.com>
2024-11-20 23:09:47 -0600

    update - material table
    - remplaced bootstrap table

--- src/app/hotel/pages/rooms-page/rooms-page.component.ts
+++ src/app/hotel/pages/rooms-page/rooms-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { FacadeService } from 'src/app/auth/services/facade.service';
@@ -6,10 +6,17 @@ import { HotelService } from '../../services/hotel.service';
 
 import { environment } from '@env/environment'
 
+// Angular Material
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+
 @Component({
   selector: 'app-rooms-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTableModule,
+    MatPaginator
+  ],
   templateUrl: './rooms-page.component.html',
   styleUrl: './rooms-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -17,12 +24,22 @@ import { environment } from '@env/environment'
 export class RoomsPageComponent implements OnInit {
   
   public mainUrl = environment.API_URL;
-  public token:string = "";
-  // public listaHabitaciones: any [] = [];
-  public listaHabitaciones: any = {};
-
   currentRoute: string | undefined;
   
+  public token:string = "";
+  public listaHabitaciones: any [] = [];
+  public role: string = "";
+  public dataColumns:string[] | undefined;
+
+  displayedColumns: string[] = [];
+  dataSourceHabitacion = new MatTableDataSource<DatosHabitacion>(this.listaHabitaciones as DatosHabitacion[]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSourceHabitacion.paginator = this.paginator;
+  }
+
   constructor(
     private router: Router,
     private facadeService: FacadeService,
@@ -35,7 +52,10 @@ export class RoomsPageComponent implements OnInit {
     if(this.token == ""){
       this.router.navigate(['']);
     }else{
+      this.role = this.facadeService.getUserGroup();
       this.obtenerHabitaciones();
+      this.initPaginator();
+      this.mostrarColumnas();
     }
   }
 
@@ -45,6 +65,10 @@ export class RoomsPageComponent implements OnInit {
         console.log(response);
         this.listaHabitaciones = response;
         console.log(this.listaHabitaciones);
+
+        if(this.listaHabitaciones.length > 0){
+          this.dataSourceHabitacion = new MatTableDataSource<DatosHabitacion>(this.listaHabitaciones as DatosHabitacion[]);
+        }
       },
       error: (error)=>{
         alert("¡Error!: Lista no Obtenida");
@@ -59,4 +83,44 @@ export class RoomsPageComponent implements OnInit {
   public goEliminar(id: number){
     this.router.navigate(['rooms/delete/'+id]);
   }
+
+  // Función Para Mostrar Columnas
+  public mostrarColumnas(){
+    if(this.role == "administrador"){
+      this.displayedColumns = ['id', 'imagen', 'número', 'tipo', 'precio', 'disponible', 'editar', 'eliminar'];
+    }else if(this.role == "recepcionista"){
+      this.displayedColumns = ['id', 'imagen', 'número', 'tipo', 'precio', 'disponible'];
+    }
+  }
+  
+
+  // Paginador
+  public initPaginator(){
+    setTimeout(() => {
+      this.dataSourceHabitacion.paginator = this.paginator;
+      this.paginator._intl.itemsPerPageLabel = 'Registros por página';
+      this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+        if (length === 0 || pageSize === 0) {
+          return `0 / ${length}`;
+        }
+        length = Math.max(length, 0);
+        const startIndex = page * pageSize;
+        const endIndex = startIndex < length ? Math.min(startIndex + pageSize, length) : startIndex + pageSize;
+        return `${startIndex + 1} - ${endIndex} de ${length}`;
+      };
+      this.paginator._intl.firstPageLabel = 'Primera página';
+      this.paginator._intl.lastPageLabel = 'Última página';
+      this.paginator._intl.previousPageLabel = 'Página anterior';
+      this.paginator._intl.nextPageLabel = 'Página siguiente';
+    },500);
+  }
+}
+
+export interface DatosHabitacion {
+  id: number;
+  numero: number;
+  tipo: string;
+  precio: number;
+  disponible: boolean;
+  imagen: string;
 }
